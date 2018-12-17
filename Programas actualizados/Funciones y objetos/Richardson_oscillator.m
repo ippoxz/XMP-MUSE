a0606aad4e11311c586cd348cf46d3401fd6c4b0
Enterprixe <44772375+Enterprixe@users.noreply.github.com>
2018-12-17 15:42:19 +0100

    Update Richardson_oscillator.m

--- Programas actualizados/Funciones y objetos/Richardson_oscillator.m
+++ Programas actualizados/Funciones y objetos/Richardson_oscillator.m
@@ -1,4 +1,4 @@
-function[W_1, W_2, U_real,Error, Error_vs_real] =  Richardson_oscillator(t,n,U_0,dt)
+function[W_1, W_2, U_real,Error, Error_vs_real] =  Richardson_oscillator(t,n,U_0,dt,pert)
 
 t_1 = linspace(0,t,n);
 
@@ -16,7 +16,7 @@ Error = zeros(1,n);
 Error_vs_real = zeros(1,n);
 
 for i = 1:n
-    U_real(i) = cos(t_1(i));
+    U_real(i) = (1+pert)*cos(t_1(i));
     Error(i) = abs(W_2(1,2*i) - W_1(1,i))*2;
     Error_vs_real(i) = abs(U_real(i)- W_1(1,i))  ;
 end
