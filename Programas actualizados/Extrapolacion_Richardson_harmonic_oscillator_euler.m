4164c0e3d8f20341181f2f056d7f00ff8d69b70b
Enterprixe <44772375+Enterprixe@users.noreply.github.com>
2018-12-17 15:42:51 +0100

    Update Extrapolacion_Richardson_harmonic_oscillator_euler.m

--- Programas actualizados/Extrapolacion_Richardson_harmonic_oscillator_euler.m
+++ Programas actualizados/Extrapolacion_Richardson_harmonic_oscillator_euler.m
@@ -1,21 +1,23 @@
 addpath ('Funciones y objetos')
 %-------------------Extrapolacion Richardson------------------------------%
 n = 2000000;
-
+pert = 1e-3;
 U_0 = zeros(2,n);
-U_0(:,1) = [1; 0]; %Condicion inicial
-t = 50;
+U_0(:,1) = [1+pert; 0]; %Condicion inicial
+t = 100;
 dt = t/n;
 t_1 = linspace(0,t,n);
 t_2 = linspace(0,t,2*n);
 
-[W_1, W_2, U_real,Error, Error_vs_real] =  Richardson_oscillator(t,n,U_0,dt);
+[W_1, W_2, U_real,Error, Error_vs_real] =  Richardson_oscillator(t,n,U_0,dt,pert);
+
+exp_Lyap = 1/t_1(n)*log(Error(n)/pert);
 
 figure
 plot(t_1,Error)
 grid on
 xlabel('$t$', 'FontSize',14,'Interpreter','latex')
-ylabel('Error entre dos numericas', 'FontSize',14,'Interpreter','latex')
+ylabel('$Error vs dos numericos$', 'FontSize',14,'Interpreter','latex')
 
 
 
@@ -23,7 +25,7 @@ figure
 plot(t_1,Error_vs_real)
 grid on
 xlabel('$t$', 'FontSize',14,'Interpreter','latex')
-ylabel('Error entre numerico y analitico', 'FontSize',14,'Interpreter','latex')
+ylabel('$Error analitica y numerico$', 'FontSize',14,'Interpreter','latex')
 
 
 figure 
